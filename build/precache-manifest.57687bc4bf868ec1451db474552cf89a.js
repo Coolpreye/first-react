4d2905f4f477a069ea1038991637286c48bc79bf
coolpreye <kerebipreye@gmail.com>
2019-05-13 16:04:47 -0700

    first commit

---
+++ build/precache-manifest.57687bc4bf868ec1451db474552cf89a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a37ce8dca8f43b78fa3",
+    "url": "/static/js/main.e9f6abd9.chunk.js"
+  },
+  {
+    "revision": "4708268b4dc145d00b83",
+    "url": "/static/js/2.329ddab1.chunk.js"
+  },
+  {
+    "revision": "0a37ce8dca8f43b78fa3",
+    "url": "/static/css/main.95ca474d.chunk.css"
+  },
+  {
+    "revision": "99fcea74b7fd669f1ef621ca086790c7",
+    "url": "/index.html"
+  }
+];
